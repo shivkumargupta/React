a15c55afdd7b8f1b17f60405d4a0e963558310e4
Shivkumar Gupta <shigupta15@publicisgroupe.net>
2018-11-14 21:10:15 +0530

    Added Chrome debuger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello World</h1>
-    );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name:"Shiv"
+    }
+  }
+
+  componentDidMount(){
+    this.loadfunction();
+  }
+
+  loadfunction = () => {
+    alert("Hello World")
+  }
+
+  render(){
+    return(
+      <h1>Hello {this.state.name}</h1>
+    )
   }
 }
-
-export default App;
